db5c65f5dc8117da27a1a7f18c73514c5781e958
Joao-victor-da-silva <75917980+Joao-victor-da-silva@users.noreply.github.com>
2025-03-08 20:06:15 -0300

    caixa pega fogo

--- scripts/cenario/mobilia.gd
+++ scripts/cenario/mobilia.gd
@@ -7,18 +7,37 @@ func _ready() -> void:
 	controle_magia(id_magia)
 	pass
 	
+func _process(delta: float) -> void:
+	$fogo/fogo.global_rotation = 0
+	pass
+	
 func controle_magia(id_magia):
 	match id_magia:
 		0:
 			print("normal")
+			$Sprite2D.modulate = Color8(255,255,255,255)
+			$fogo.visible = false
 			set_collision_mask_value(8, false) 
 		1:
 			print("fogo")
+			$Sprite2D.modulate = Color8(255,56,25,255)
+			$fogo/areaMagia.visible = true
+			$fogo.visible = true
 			set_collision_mask_value(8, true)
 		2:
 			print("gelo")
+			$Sprite2D.modulate = Color8(137,255,255,255)
+			$fogo.visible = false
 			set_collision_mask_value(8, true)
 		3: 
 			print("telecinese")
+			$Sprite2D.modulate = Color8(232,0,248,255)
+			$fogo.visible = false
 			set_collision_mask_value(8, true)
 	pass 
+
+
+func _on_fogo_body_entered(body: Node2D) -> void:
+	if id_magia == 1:
+		body.controle_magia(id_magia)
+	pass # Replace with function body.
